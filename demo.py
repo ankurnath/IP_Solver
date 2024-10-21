cf7c504889e40e86059f9ae5957bb7f326764ac8
Ankur Nath <anath@csce-44q58y3.engr.tamu.edu>
2024-10-21 10:56:14 -0500

    Completed

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from utils import *
+import matplotlib.ticker as ticker
+
 
 
 
@@ -23,21 +25,56 @@ for i,dist in enumerate(['ER','BA']):
             df['algorithm'].append(algorithm)
             df ['Ratio'].append((df_['cut']/df_['OPT']).mean())
 
+    #### CPLEX
+    size = [20,40,60,100,200,500]
+    df['N'] += size
+    df['algorithm'] += ['CPLEX*']* len(size)
+    if dist =='ER':
+        df ['Ratio'] += [1,1,1,0.87,0.46,0.16]
+    elif dist == 'BA':
+        df ['Ratio'] += [1,1,1,1,0.83,0.17]
+
+
 
     df = pd.DataFrame(df)
 
-    
+    fontsize = 20
     plt.figure(dpi=200)
-    markers = {'Gurobi': 'p', 'Greedy': 'P', 'TS': 'D'}  # specify markers for each algorithm
-    sns.lineplot(x='N', y='Ratio', hue='algorithm', style='algorithm', markers=markers, data=df, markersize=10)
+    markers = {'Gurobi': 'p', 'Greedy': 'P', 'TS': 'D','CPLEX*':'*'}  # specify markers for each algorithm
+    sns.lineplot(x='N', y='Ratio', hue='algorithm', style='algorithm', markers=markers, data=df, markersize=20)
+    
+    plt.xlabel('Graph Size,|V|',fontsize=fontsize)
+    plt.ylabel('Approx. Ratio',fontsize=fontsize)
 
+    plt.xticks([20,40,60,100,200,500],fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    
 
+    # plt.locator_params(nbins=6)
+    # plt.xticks([20,40,60,100,200,500])
+    plt.xscale('log')
+    # Set custom xticks on a logarithmic scale using LogLocator
+    ax = plt.gca()  # Get current axis
+    ax.set_xticks([20, 40, 60, 100, 200, 500])  # Specify the exact tick locations
+    ax.get_xaxis().set_major_formatter(ticker.ScalarFormatter())  # Ensure the tick labels are formatted properly
+    # plt.xticks([20,40,60,100,200,500])
+
+    # ax = plt.gca()  # Get the current axis
+    # ax.xaxis.set_major_locator(ticker.LogLocator(base=10.0, subs=[0.0,1.0, 2.0, 5.0]))  # Major ticks at log scale
+
+    
+    # plt.grid(True,linestyle='--', alpha=0.7)
+    plt.legend(frameon=False,fontsize=fontsize)
+
+    plt.savefig(f'{distribution}', bbox_inches='tight')
+    plt.savefig(f'{distribution}.pdf', bbox_inches='tight')
+    # plt.show()
     
     # ax.plot(df['N'], df['Ratio'], label=algorithm)
     # print(df)
 
-plt.legend()
-plt.show()
+
+
 
 
 
